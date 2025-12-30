98acec3a9e5d18b00153e8fb3d8e4f17baa51973
awais512 <raza.awais25@gmail.com>
2025-12-30 21:00:36 +0000

    Removed rating component

--- features/movies/index.ts
+++ features/movies/index.ts
@@ -5,7 +5,6 @@ export * from './components/movie-info';
 export * from './components/movie-overview';
 export * from './components/movie-cast';
 export * from './components/movie-recommendations';
-export { default as MovieActionsMenu } from './components/movie-actions-menu';
 export * from './components/movie-rating-badge';
 export * from './components/movies-page-client';
 
